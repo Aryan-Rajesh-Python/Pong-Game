71d0b950304333e1a4ae8af2de5eb066d2e429c9
Aryan Rajesh <aryanrajesh6702@gmail.com>
2023-02-11 12:16:28 +0530

    Add files via upload

---
+++ pongmain.py
@@ -0,0 +1,35 @@
+from turtle import Screen,Turtle
+from paddle import Paddle
+from ball import Ball
+from scoreboard1 import Scoreboard
+import time
+screen=Screen()
+screen.bgcolor("black")
+screen.setup(width=800,height=600)
+screen.title("Pong")
+screen.tracer(0)
+r_paddle=Paddle((350,0))
+l_paddle=Paddle((-350,0))
+ball=Ball()
+scoreboard=Scoreboard()
+screen.listen()
+screen.onkey(r_paddle.go_up,"u")
+screen.onkey(r_paddle.go_down,"d")
+screen.onkey(l_paddle.go_up,"w")
+screen.onkey(l_paddle.go_down,"s")
+game_on=True
+while(game_on):
+    time.sleep(0.01)
+    screen.update()
+    ball.move()
+    if(ball.ycor()>280 or ball.ycor()<-280):
+        ball.bounce_y()
+    if(ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320):
+        ball.bounce_x()
+    if(ball.xcor()>380):
+        ball.reset_position()
+        scoreboard.l_point()
+    if(ball.xcor()<-380):
+        ball.reset_position()
+        scoreboard.r_point()
+screen.exitonclick()
